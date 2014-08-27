b595b7b7d19687bf149521c9ddcf3c6853ce2087
AndrienkoAleksandr <logarifm6@gmail.com>
2014-08-27 18:26:12 +0300

    Added SimplePager

--- src/main/java/com/codenvy/employee/client/view/impl/EditUserDialogBoxViewViewImpl.java
+++ src/main/java/com/codenvy/employee/client/view/impl/EditUserDialogBoxViewViewImpl.java
@@ -7,10 +7,12 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
+import sun.java2d.pipe.SpanShapeRenderer;
 
 /**
  * Created by Andrienko Alexander  on 19.08.14.
--- src/main/java/com/codenvy/employee/client/view/impl/UsersListViewImpl.java
+++ src/main/java/com/codenvy/employee/client/view/impl/UsersListViewImpl.java
@@ -9,10 +9,13 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.AsyncDataProvider;
+import com.google.gwt.view.client.HasData;
 import com.google.gwt.view.client.SelectionChangeEvent;
 import com.google.gwt.view.client.SingleSelectionModel;
 
@@ -27,6 +30,9 @@ public class UsersListViewImpl extends Composite implements UsersListView {
     }
 
     private UsersListPresenter usersListPresenter;
+
+    @UiField
+    SimplePager pager;
 
     @UiField
     CellTable<User> usersTable;
@@ -53,8 +59,23 @@ public class UsersListViewImpl extends Composite implements UsersListView {
     }
 
     @Override
-    public void setUsers(List<User> users) {
-        usersTable.setRowData(users);
+    public void setUsers(final List<User> users) {
+        final AsyncDataProvider<User> provider = new AsyncDataProvider<User>() {
+            @Override
+            protected void onRangeChanged(HasData<User> display) {
+                int start = display.getVisibleRange().getStart();
+                int end = start + display.getVisibleRange().getLength();
+                end = end >= users.size() ? users.size() : end;
+                List<User> sub = users.subList(start, end);
+                updateRowData(start, sub);
+                GWT.log(start + " " + end);
+            }
+        };
+        provider.addDataDisplay(usersTable);
+        provider.updateRowCount(users.size(), true);
+
+        pager.setDisplay(usersTable);
+//        usersTable.setRowData(users);
     }
 
     public void drawUserTable() {
